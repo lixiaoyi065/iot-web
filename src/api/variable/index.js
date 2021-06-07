b8c8b14ae1388612aac304b9beb46091d79b4ae7
Lxyi <1219968638@qq.com>
2021-06-07 20:07:28 +0800

    变量管理接口

--- src/api/variable/index.js
+++ src/api/variable/index.js
@@ -6,3 +6,23 @@ export function getEquList() {
     method: 'get'
   })
 }
+
+export function addGroup(group) {
+  return service({
+    url: '/api/VariableManage/AddGroup',
+    params: {
+      ...group
+    },
+    method: 'post'
+  })
+}
+
+export function addEqu(group) {
+  return service({
+    url: '/api/VariableManage/AddEqu',
+    params: {
+      ...group
+    },
+    method: 'post'
+  })
+}
